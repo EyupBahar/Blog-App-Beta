253eaecb7625feb9fa8618e8f9b34ac9290b726d
7204-OMER <Eyupbhr24@gmail.com>
2021-08-27 13:48:57 +0300

    add dashboard changes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,6 +4,8 @@ import PostContainer from "../components/PostContainer";
 const Dashboard = () => {
   return (
     <div>
+      <p>Sorry About that, This is a problem with github deploying and You can access the project with this link below. Thank you for your understanding </p>
+      <p>https://blog-app-beta-version.herokuapp.com/</p>
       <PostContainer />
     </div>
   );
